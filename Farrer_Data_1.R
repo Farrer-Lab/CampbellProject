b247b2b326cb977d2f585690a6acf4dd1426585a
Campbell Reid <campbell.reid@Campbells-MacBook-Air-4.local>
2025-03-20 16:06:43 -0500

    changed graphs and added confidence intervals

--- Farrer_Data_1.R
+++ Farrer_Data_1.R
@@ -8,29 +8,29 @@ farrer$Stipul #look at this particular document
 farrer[15] #look at this particular column
 farrer[12,15]#return 12th row of 15th column
 farrer[which(is.na(farrer$AV)),12]<-0 #replace N/As in column with "0"
-farrer[which(is.na(farrer$MH)),11]<-0 #replace N/As in column with "0"
-farrer[which(is.na(farrer$Placal)), 14]<-0 #replace N/As in column with "0"
-farrer[which(is.na(farrer$Stipul)), 15]<-0 #replace N/As in column with "0"
-farrer[which(is.na(farrer$Triwil)), 13]<-0 #replace N/As in column with "0"
-farrer[which(is.na(farrer$Legumeweed)),18]<-0 #replace N/As in column with "0"
+farrer[which(is.na(farrer$MH)),11]<-0 
+farrer[which(is.na(farrer$Placal)), 14]<-0 
+farrer[which(is.na(farrer$Stipul)), 15]<-0 
+farrer[which(is.na(farrer$Triwil)), 13]<-0 
+farrer[which(is.na(farrer$Legumeweed)),18]<-0 
 farrer[248,11]<-0 #replace column and row spot with 0
 farrer[117,11]<-8.31 #replace column and row spot with 8.31
 farrer[95,11]<-0 #replace column and row spot with 0
-farrer$Natives<-farrer$Stipul + farrer$Triwil + farrer$Placal #make new folder of sum of these columns
-farrer$Invasives<-as.numeric(farrer$AV) + as.numeric(farrer$MH) #make new folder of sum of these columns, as.numeric means "read as number"
-farrer$Weeds<-as.numeric(farrer$Grassweed) + as.numeric(farrer$Forbweed) + as.numeric(farrer$Legumeweed) #make new folder of sum of these columns, as.numeric means "read as number"
-farrer$TotalPlantBiomass<-farrer$Natives + farrer$Weeds + farrer$Invasives #make new folder of sum of these columns
-farrer$PercentNatives<-farrer$Natives/farrer$TotalPlantBiomass
-farrer$PercentInvasives<-farrer$Invasives/farrer$TotalPlantBiomass
-farrer$PercentWeeds<-farrer$Weeds/farrer$TotalPlantBiomass
-farrer$Seed<-0
-farrer$Seed[which(farrer$Treatment=="W")]<-"Orange" 
+farrer$Natives<-farrer$Stipul + farrer$Triwil + farrer$Placal #make new column which is the sum of these columns
+farrer$Invasives<-as.numeric(farrer$AV) + as.numeric(farrer$MH) #make new column which is the sum of these columns, as.numeric means "read as number"
+farrer$Weeds<-as.numeric(farrer$Grassweed) + as.numeric(farrer$Forbweed) + as.numeric(farrer$Legumeweed) 
+farrer$TotalPlantBiomass<-farrer$Natives + farrer$Weeds + farrer$Invasives 
+farrer$PercentNatives<-farrer$Natives/farrer$TotalPlantBiomass #make new column which value of the natives divided by the total plant biomass
+farrer$PercentInvasives<-farrer$Invasives/farrer$TotalPlantBiomass #make new column which value of the invasives divided by the total plant biomass
+farrer$PercentWeeds<-farrer$Weeds/farrer$TotalPlantBiomass #make new column which value of the weeds divided by the total plant biomass
+farrer$Seed<-0 #make this column and fill it with zero
+farrer$Seed[which(farrer$Treatment=="W")]<-"Orange" #replace the zero in this column with "orange" if its treatment is this seed type
 farrer$Seed[which(farrer$Treatment=="Z")]<-"Orange"
 farrer$Seed[which(farrer$Treatment=="c")]<-"Orange"
-farrer$Seed[which(farrer$Treatment=="X")]<-"Pink"
+farrer$Seed[which(farrer$Treatment=="X")]<-"Pink" #replace the zero in this column with "pink" if its treatment is this seed type
 farrer$Seed[which(farrer$Treatment=="a")]<-"Pink"
 farrer$Seed[which(farrer$Treatment=="d")]<-"Pink"
-farrer$Seed[which(farrer$Treatment=="V")]<-"Yellow"
+farrer$Seed[which(farrer$Treatment=="V")]<-"Yellow" #replace the zero in this column with "yellow" if its treatment is this seed type
 farrer$Seed[which(farrer$Treatment=="Y")]<-"Yellow"
 farrer$Seed[which(farrer$Treatment=="b")]<-"Yellow"
 
@@ -44,41 +44,49 @@ summary(RainNova.Weeds) #rain is significant on percent weeds
 LitterNova.Natives<-aov(PercentNatives~litter, data=farrer)
 summary(LitterNova.Natives) #litter is significant on percent natives
 TukeyHSD(LitterNova.Natives)#all three comparisons are significant
+plot(TukeyHSD(LitterNova.Natives, conf.level = .95),las=2) #medium litter average natives > high litter, no litter average natives > high litter, no litter average natives > medium litter
 LitterNova.Invasives<-aov(PercentInvasives~litter, data=farrer)
 summary(LitterNova.Invasives)#litter is significant on percent invasives
 TukeyHSD(LitterNova.Invasives)#all three comparisons are significant
+plot(TukeyHSD(LitterNova.Invasives, conf.level = .95),las=2) #high litter average invasives > medium litter average invasives > no litter average invasives
 LitterNova.Weeds<-aov(PercentWeeds~litter, data=farrer)
 summary(LitterNova.Weeds) #litter is significant on percent weeds
 TukeyHSD(LitterNova.Weeds) #there is only a significant difference between none and high litter
+plot(TukeyHSD(LitterNova.Weeds, conf.level = .95),las=2)#medium litter average weeds > hgih litter average weeds, no litter average weeds > high litter, no litter average weeds > medium litter
 SoilNova.Natives<-aov(PercentNatives~P1, data=farrer)
 summary(SoilNova.Natives) #soil type is significant on percent natives
 TukeyHSD(SoilNova.Natives)#Difference between MH and AV, MH_L and AV, MH and MH_L is different for soil compared to AV and C, AV is similar to control
+plot(TukeyHSD(SoilNova.Natives, conf.level = .95),las=2) #mostly positive differences in means, see plot for specifics
 SoilNova.Invasives<-aov(PercentInvasives~P1, data=farrer)
 summary(SoilNova.Invasives)#soil type is significant on percent invasives
-TukeyHSD(SoilNova.Invasives) #soil type is not hugley important for Invasive, soil type is significant on percent weeds, MH_L was significantly different from all that aern't MH
+TukeyHSD(SoilNova.Invasives) #soil type is not hugely important for Invasive, soil type is significant on percent weeds, MH_L was significantly different from all that are not MH
+plot(TukeyHSD(SoilNova.Invasives, conf.level = .95),las=2)#mostly negative differences in means, except one, see plot for specifics
 SoilNova.Weeds<-aov(PercentWeeds~P1, data=farrer)
-summary(SoilNova.Weeds)
-TukeyHSD(SoilNova.Weeds)#also significant difference is MH_L vs. AV
+summary(SoilNova.Weeds) #soil type is significant on percent weeds
+TukeyHSD(SoilNova.Weeds) #only significant difference is MH_L vs. AV
+plot(TukeyHSD(SoilNova.Weeds, conf.level = .95),las=2) #mix between negative and positive, but mostly positive, see plot for specifics
 NativeNova<-aov(PercentNatives~litter*P1*Rain, data=farrer)
-summary(NativeNova)
+summary(NativeNova) #litter, soil, interaction between litter*soil, and interaction between litter*soil*rain are important
 TukeyHSD(NativeNova)
 NativeTukey<-TukeyHSD(NativeNova)
 InvasivesNova<-aov(PercentInvasives~litter*P1*Rain, data=farrer)
-summary(InvasivesNova)#P1, Litter, and Rain are significant but not interactions
+summary(InvasivesNova)#P1, Litter, and Rain are significant but the interactions are not significant
 WeedsNova<-aov(PercentWeeds~litter*P1*Rain, data=farrer)
-summary(WeedsNova)#P1, Litter, and Rain are significant but not interactions #Orange means MH added, 
+summary(WeedsNova)#P1, Litter, and Rain are significant but not interactions #Orange means MH added 
 SeedNova.Invasives<-aov(PercentInvasives~Seed, data=farrer)
-summary(SeedNova.Invasives)
-SeedNova.Natives<-aov(PercentNatives~Seed, data=farrer)
-summary(SeedNova.Natives)
+summary(SeedNova.Invasives) #seed is not significant on invasives
+SeedNova.Natives<-aov(PercentNatives~Seed, data=farrer) 
+summary(SeedNova.Natives) #seed is not significant on natives
 TukeyHSD(SeedNova.Natives)#the difference between yellow and pink was near significant
-SeedNova.Weeds<-aov(PercentWeeds~Seed, data=farrer)
-summary(SeedNova.Weeds)
+SeedNova.Weeds<-aov(PercentWeeds~Seed, data=farrer) 
+summary(SeedNova.Weeds) #seed is not significant on weeds
 
 #####Graphs#####
-library(ggplot2)
-ggplot(data=farrer, aes(x=P1, y=PercentNatives))+geom_bar(stat="identity", position="dodge")+theme_bw()
-ggplot(data=farrer, aes(x=litter, y=PercentNatives))+geom_bar(stat="identity", position="dodge")+theme_bw()
-ggplot(data=farrer, aes(x=Rain, y=PercentInvasives))+geom_bar(stat="identity", position="dodge")+theme_bw()
-ggplot(data=farrer, aes(x=Rain, y=PercentWeeds))+geom_bar(stat="identity", position="dodge")+theme_bw()
-ggplot(data=farrer, aes(x=P1, y=PercentNatives, fill=litter))+geom_bar(stat="identity", position="dodge")+theme_bw()
+library(ggplot2) #enable ggplot add-on
+ggplot(data=farrer, aes(x=P1,y=PercentNatives))+geom_bar(stat="summary", position="dodge", fun="mean")+theme_bw() #make a bar graph with x being soil treatment and y being percent natives
+ggplot(data=farrer, aes(x=litter, y=PercentNatives))+geom_bar(stat="summary", position="dodge", fun="mean")+theme_bw()
+ggplot(data=farrer, aes(x=Rain, y=PercentInvasives))+geom_bar(stat="summary", position="dodge", fun="mean")+theme_bw()
+ggplot(data=farrer, aes(x=Rain, y=PercentWeeds))+geom_bar(stat="summary", position="dodge", fun="mean")+theme_bw()
+ggplot(data=farrer, aes(x=P1, y=PercentNatives, fill=litter))+geom_bar(stat="summary", position="dodge", fun="mean")+theme_bw() #make a bar graph with x being soil treatment and y being percent natives and include differentiation based on litter
+ggplot(data=farrer, aes(x=Rain, y=PercentInvasives))+geom_bar(stat="summary", position= "dodge", fun="mean")+theme_bw()
+ggplot(data=farrer, aes(x=P1, y=PercentInvasives))+geom_bar(stat="summary", position="dodge", fun="mean")+theme_bw()
